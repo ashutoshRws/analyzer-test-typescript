b40d8b2e05222168fd212aaf76b8870b2f17a7bb
Ashutosh Patidar <Ashutosh.Patidar@inindcrws.com>
2026-05-18 14:12:12 +0530

    Fix repo layout and add security probes

--- src/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { greet } from './utils';
-import { createUser } from './models/user';
-
-function main(): void {
-  const user = createUser('tester');
-  console.log(greet(user.name));
-}
-
-main();
--- src/src/models/user.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-  id: number;
-  name: string;
-}
-
-export function createUser(name: string): User {
-  return { id: 1, name };
-}
